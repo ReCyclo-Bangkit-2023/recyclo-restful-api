5e997c0987e596f6d50a00f1b5c950dee81dc808
Syahda Romansyah <syahdarom@gmail.com>
2023-05-27 21:13:11 +0700

    feat: initialize template handlers for wastes route

--- src/handlers/recyclo-handlers.ts
+++ src/handlers/recyclo-handlers.ts
@@ -1,1 +1,6 @@
-export {};
+import addWaste from './recyclo-handlers/waste-handlers/add-waste.js';
+import deleteWaste from './recyclo-handlers/waste-handlers/delete-waste.js';
+import getWastes from './recyclo-handlers/waste-handlers/get-wastes.js';
+import putWaste from './recyclo-handlers/waste-handlers/put-waste.js';
+
+export { getWastes, addWaste, putWaste, deleteWaste };

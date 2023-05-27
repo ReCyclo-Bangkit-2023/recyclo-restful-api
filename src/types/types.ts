e0d2d389b1e2b302b277a946504a99d2edd6e9ff
Syahda Romansyah <syahdarom@gmail.com>
2023-05-28 00:46:52 +0700

    feat: create new types on types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import Jwt from '@hapi/jwt';
+
 export interface UserDataDocProps {
   userId: string;
   fullname: string;
@@ -14,3 +16,11 @@ export type LoginRequestBodyProps = Pick<
   UserDataDocProps,
   'email' | 'password'
 >;
+
+export interface DecodedTokenPayloadProps {
+  userId: string;
+  iat: number;
+  exp: number;
+}
+
+export type JwtTokenVerifyOpt = Parameters<typeof Jwt.token.verify>[2];

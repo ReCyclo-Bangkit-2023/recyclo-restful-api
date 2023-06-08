dcbafde427ce71635528604b16c756e4ae6003dd
Adisti Dwi Susanti <adistidwis20@if.unjani.ac.id>
2023-06-08 09:13:15 +0700

    code has not been integrated with the database

--- src/handlers/recyclo-handlers/transaction-handlers/get-transactions.ts
+++ src/handlers/recyclo-handlers/transaction-handlers/get-transactions.ts
@@ -1,10 +1,58 @@
-import type { ReqRefDefaults, Request, ResponseToolkit } from '@hapi/hapi';
+import { Request, ResponseToolkit } from '@hapi/hapi';
 
-const getTransactions = (
-  request: Request<ReqRefDefaults>,
-  h: ResponseToolkit<ReqRefDefaults>
-) => {
-  return h.response('implement me');
+interface TransactionProps {
+  transactionId: string;
+  userId: string;
+  title: string;
+  fee: number;
+  itemPrice: number;
+  amount: number;
+  statusTransaction: string;
+  date: string;
+}
+const transactions: TransactionProps[] = [
+  {
+    transactionId: '001',
+    userId: 'user1',
+    title: 'Kerajinan Rotan',
+    fee: 5000,
+    itemPrice: 1000000,
+    amount: 1005000,
+    statusTransaction: 'Delivered',
+    date: new Date().toISOString(),
+  },
+  {
+    transactionId: '002',
+    userId: 'user2',
+    title: 'Kerajinan Plastik',
+    fee: 5000,
+    itemPrice: 50000,
+    amount: 55000,
+    statusTransaction: 'In Delivery Process',
+    date: new Date().toISOString(),
+  },
+];
+
+const getTransactions = (request: Request, h: ResponseToolkit) => {
+  const { userId } = request.params;
+  const index = transactions.filter(
+    (transaction) => transaction.userId === userId
+  ); // memilih userId yang akan ditampilkan
+
+  if (index) {
+    return {
+      status: 'success',
+      data: {
+        index,
+      },
+    };
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'transaction not found',
+  });
+  response.code(404);
+  return response;
 };
 
 export default getTransactions;
--- src/handlers/recyclo-handlers/transaction-handlers/put-transaction.ts
+++ src/handlers/recyclo-handlers/transaction-handlers/put-transaction.ts
@@ -1,15 +1,80 @@
-import type { ReqRefDefaults, Request, ResponseToolkit } from '@hapi/hapi';
+import { Request, ResponseToolkit } from '@hapi/hapi';
+
+/*interface TransactionProps {
+  transactionId: string;
+  userId: string; // seller
+  statusTransaction: string;
+  date: string;
+}*/
+
+const transactions = [
+  {
+    transactionId: '001',
+    userId: 'user1',
+    statusTransaction: 'Berhasil Dikirim',
+    date: new Date().toISOString(),
+  },
+  {
+    transactionId: '002',
+    userId: 'user2',
+    statusTransaction: 'Belum Dikirim',
+    date: new Date().toISOString(),
+  },
+];
 
 export const confirmSellerTransaction = (
-  request: Request<ReqRefDefaults>,
-  h: ResponseToolkit<ReqRefDefaults>
+  request: Request,
+  h: ResponseToolkit
 ) => {
-  return h.response('implement me');
+  const { transactionId } = request.params;
+  const { statusTransaction } = request.payload as {
+    statusTransaction: string;
+  };
+  const transaction = transactions.find(
+    (t) => t.transactionId === transactionId
+  );
+  if (transaction) {
+    transaction.statusTransaction = statusTransaction;
+    return {
+      status: 'success',
+      transactionId: transaction.transactionId,
+      message: 'Seller transaction confirmed successfully',
+    };
+  }
+  return h
+    .response({
+      status: 'fail',
+      message: 'Transaction not found',
+    })
+    .code(404);
 };
 
+//orderCompleteTransaction
 export const orderCompleteTransaction = (
-  request: Request<ReqRefDefaults>,
-  h: ResponseToolkit<ReqRefDefaults>
+  request: Request,
+  h: ResponseToolkit
 ) => {
-  return h.response('implement me');
+  const { transactionId } = request.params;
+  const { id, statusOrder } = request.payload as {
+    id: string;
+    statusOrder: string;
+  };
+  const transaction = transactions.find(
+    (t) => t.transactionId === transactionId
+  );
+  if (transaction) {
+    transaction.statusTransaction = statusOrder; //kalau pembayaran sudah terverifikasi maka selesai
+    return {
+      status: 'success',
+      transactionId: id,
+      message: 'Order completed successfully',
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Order not found',
+  });
+  response.code(404);
+  return response;
 };

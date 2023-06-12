33bc0c637055b867e69c45668c86c3346ea7a767
Syahda Romansyah <syahdarom@gmail.com>
2023-06-12 17:02:07 +0700

    feat: add Cloud Firestore recycledItems collection config key

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 const config = {
   JWT_KEY: process.env.JWT_KEY as string,
   CLOUD_STORAGE_BUCKET_NAME: 'recyclo-387407-bucket',
+  CLOUD_FIRESTORE_RECYCLED_ITEMS_COLLECTION: 'recycledItems',
 };
 
 export default config;
